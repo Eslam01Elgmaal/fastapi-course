fe70d1f0ebf84abacc9ae7a0e7e7f0aa7dd2e4f6
Eslam01Elgmaal <Eslam.Plutaki@gmail.com>
2026-08-29 16:13:40 +0300

    Configure database URL for deployment

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,11 +1,21 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
+SQLALCHEMY_DATABASE_URL = os.getenv(
+    "DATABASE_URL",
+    "postgresql://postgres:26622272@localhost:5432/TodoApplicationsDatabase"
+)
 
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:26622272@localhost:5432/TodoApplicationsDatabase'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit= False, autoflush=False, bind=engine)
-Base = declarative_base()
 
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()

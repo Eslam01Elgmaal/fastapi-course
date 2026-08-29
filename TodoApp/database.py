81ebf430269db29254d3c32564fb7371f6976412
Eslam01Elgmaal <Eslam.Plutaki@gmail.com>
2026-08-29 16:34:28 +0300

    back to sqlite

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -7,10 +7,13 @@ from sqlalchemy.ext.declarative import declarative_base
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
     "DATABASE_URL",
-    "postgresql://postgres:26622272@localhost:5432/TodoApplicationsDatabase"
+    "sqlite:///./todoapp.db"
 )
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,

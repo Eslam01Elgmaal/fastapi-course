9b16a3933ffa9164b39a492c86485572ce6f9cf4
Eslam01Elgmaal <Eslam.Plutaki@gmail.com>
2026-08-29 16:45:04 +0300

    Fix static files path

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -4,11 +4,19 @@ from .database import engine
 from TodoApp.routers import auth, todos, admin, users
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import RedirectResponse
+from pathlib import Path
 
 app = FastAPI()
-Base.metadata.create_all(bind=engine)
-app.mount("/static", StaticFiles(directory="TodoApp/static"), name= "static")
 
+Base.metadata.create_all(bind=engine)
+
+BASE_DIR = Path(__file__).resolve().parent
+
+app.mount(
+    "/static",
+    StaticFiles(directory=BASE_DIR / "static"),
+    name="static"
+)
 @app.get("/")
 def main_rout(request: Request):
     return RedirectResponse(url="/todos/todo-page", status_code=status.HTTP_302_FOUND)

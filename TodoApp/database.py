d1387b09123b14f859c226c6d1ec3e9982e2022e
Eslam01Elgmaal <Eslam.Plutaki@gmail.com>
2026-08-30 13:27:02 +0300

    new PostgreSql with Neno database Production

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -5,15 +5,25 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 
-SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "sqlite:///./todoapp.db"
-)
+
+SQLALCHEMY_DATABASE_URL = "postgresql://neondb_owner:npg_J4aiUvQrkIE9@ep-fancy-surf-b1pba6h6-pooler.c-5.eu-central-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL,
-    connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
 )
+
+
+
+
+# SQLALCHEMY_DATABASE_URL = os.getenv(
+#     "DATABASE_URL",
+#     "sqlite:///./todoapp.db"
+# )
+#
+# engine = create_engine(
+#     SQLALCHEMY_DATABASE_URL,
+#     connect_args={"check_same_thread": False})
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
